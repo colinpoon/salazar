54f6afe9dd7722ecbef8fd19f160504b285b87c3
Colin Poon <me@colinpoon.com>
2023-11-25 16:30:13 -0800

    clark auth init & routes

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@/components/ui/button';
+import { Link } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center gap-5 h-full">
       <p className="text-6xl text-green-500">
         Salazar Index (Unprotected)
       </p>
+      <div>
+        <Link>
+          <Button variant={'default'}>Sign In</Button>
+        </Link>
+      </div>
     </div>
   );
 }
