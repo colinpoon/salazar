d4bb035a4601416acba212c69420f91b756be216
Colin Poon <me@colinpoon.com>
2023-11-25 17:04:39 -0800

    tailwindcss test

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,20 +1,19 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <div className="flex flex-col items-center gap-5 pt-20 px-20 h-full">
       <div className="flex flex-row justify-end items-center gap-5 w-full">
         <Link href="/sign-in">
-          <Button variant={'default'}>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button variant={'default'}>Register</Button>
+          <Button variant={'default'}>Logout</Button>
         </Link>
       </div>
-      <p className="text-6xl text-black-500 font-semibold">Index</p>
+      <p className="text-6xl text-black-500 font-semibold">
+        Dashboard Page
+      </p>
       <p className="text-3xl text-black-500 font-bold">
-        (Public / Unprotected)
+        (Private / Protected)
       </p>
     </div>
   );
