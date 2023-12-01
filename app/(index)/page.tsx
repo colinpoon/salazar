e3d2d0450468e0f22bb55d9a613aaf3b997aeacf
Colin Poon <me@colinpoon.com>
2023-11-30 16:42:44 -0800

    updated readme

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-5 px-12 py-12 h-full">
+    <div className="flex flex-col items-center px-12 py-12 h-full">
       <div className="flex flex-row justify-end items-center gap-5 w-full">
         <Link href="/sign-in">
           <Button variant={'default'}>Login</Button>
@@ -13,7 +13,7 @@ export default function Home() {
           <Button variant={'default'}>Register</Button>
         </Link>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col gap-5 items-center">
         <p className="text-6xl text-black-500 font-semibold">Index</p>
         <p className="text-3xl text-black-500 font-bold">
           (Public / Unprotected)
