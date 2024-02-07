b2f328e4598a4a918646de695386d9234ff63fc6
Colin Poon <me@colinpoon.com>
2024-02-06 20:42:43 -0800

    pro model init

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ const DashboardLayout = async ({
 
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] drop-shadow-md bg-neutral-1 border-r-[1px] border-neutral-1">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[-2] drop-shadow-md bg-neutral-1 border-r-[1px] border-neutral-1">
         <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72 ">
---
+++ hooks/use-pro.tsx
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+interface ProModelStore {
+  isAvailable: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export const usePro = create<ProModelStore>((set) => ({
+  isAvailable: false,
+  onOpen: (): void => set({ isAvailable: true }),
+  onClose: (): void => set({ isAvailable: false }),
+}));
