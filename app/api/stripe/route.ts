85934611ef5f8be90844668a447a28effec1b89d
Colin Poon <me@colinpoon.com>
2024-02-06 23:14:50 -0800

    stripe api

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -5,3 +5,32 @@ import { auth, currentUser } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 const settingsUrl = absoluteUrl('/settings');
+
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+    if (!userId || !user) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+    const userSubscription =
+      await prismadb.userSubscription.findUnique({
+        where: {
+          userId,
+        },
+      });
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession =
+        await stripe.billingPortal.sessions.create({
+          customer: userSubscription.stripeCustomerId,
+          return_url: settingsUrl,
+        });
+      return new NextResponse(
+        JSON.stringify({ url: stripeSession.url })
+      );
+    }
+  } catch (error) {
+    console.log('[STRIPE_ERROR]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
