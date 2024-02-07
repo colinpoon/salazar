e4026943f6ee56a21e275bd3ebc66353befc5673
Colin Poon <me@colinpoon.com>
2024-02-06 23:23:56 -0800

    stripe checkout

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -29,6 +29,37 @@ export async function GET() {
         JSON.stringify({ url: stripeSession.url })
       );
     }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: 'USD',
+            product_data: {
+              name: 'Salazar Pro',
+              description: `Unlimited AI Usage`,
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: 'month',
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    });
+    return new NextResponse(
+      JSON.stringify({ url: stripeSession.url })
+    );
   } catch (error) {
     console.log('[STRIPE_ERROR]', error);
     return new NextResponse('Internal Error', { status: 500 });
